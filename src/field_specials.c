562db4a9df5c452b0e208801747ba7f7f158dd92
Lewis <lewismcyoutube@lewismcyoutube.uk>
2020-02-20 13:47:28 +0000

    Update field_specials.c

--- src/field_specials.c
+++ src/field_specials.c
@@ -1737,7 +1737,7 @@ void SetShoalItemFlag(u16 v0)
 void PutZigzagoonInPlayerParty(void)
 {
     u16 monData;
-    CreateMon(&gPlayerParty[0], SPECIES_ZIGZAGOON, 7, 0x20, FALSE, 0, FALSE, 0);
+    CreateMon(&gPlayerParty[0], SPECIES_AZURILL, 7, 0x20, FALSE, 0, FALSE, 0);
     monData = TRUE;
     SetMonData(&gPlayerParty[0], MON_DATA_ALT_ABILITY, &monData);
     monData = MOVE_TACKLE;
@@ -1746,6 +1746,8 @@ void PutZigzagoonInPlayerParty(void)
     SetMonData(&gPlayerParty[0], MON_DATA_MOVE2, &monData);
     SetMonData(&gPlayerParty[0], MON_DATA_MOVE3, &monData);
     SetMonData(&gPlayerParty[0], MON_DATA_MOVE4, &monData);
+    monData = "AZURILL";
+    SetMonData(&gPlayerParty[0], MON_DATA_NICKNAME, &monData)
 }
 
 bool8 IsStarterInParty(void)
